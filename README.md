b46568b72175205f31ccf2d3f0c583d0905983eb
tanhoang0710 <tanhoang0710@gmail.com>
2022-10-09 23:59:36 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Bài tập lớn môn Lập trình mạng
---
+++ src/component/MenuButton.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package component;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author DELL
+ */
+public class MenuButton extends JButton{
+
+    public MenuButton() {
+        setContentAreaFilled(false);
+    }
+    
+}
