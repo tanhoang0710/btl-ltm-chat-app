5add86c4ff03e22d640e06753e900467565fe5d6
tanhoang0710 <tanhoang0710@gmail.com>
2022-10-10 23:46:35 +0700

    css menu-left and set select menu animation

--- src/component/MenuButton.java
+++ src/component/MenuButton.java
@@ -5,6 +5,9 @@
  */
 package component;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.Icon;
 import javax.swing.JButton;
 
 /**
@@ -13,8 +16,47 @@ import javax.swing.JButton;
  */
 public class MenuButton extends JButton{
 
+    public Icon getIconSimple() {
+        return iconSimple;
+    }
+
+    public void setIconSimple(Icon iconSimple) {
+        this.iconSimple = iconSimple;
+    }
+
+    public Icon getIconSelected() {
+        return iconSelected;
+    }
+
+    public void setIconSelected(Icon iconSelected) {
+        this.iconSelected = iconSelected;
+    }
+    private Icon iconSimple;
+    private Icon iconSelected;
+
     public MenuButton() {
         setContentAreaFilled(false);
+        setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
     }
+
+    @Override
+    public void setSelected(boolean bln) {
+        super.setSelected(bln); //To change body of generated methods, choose Tools | Templates.
+        if(bln) {
+            setIcon(iconSelected);
+        }else {
+            setIcon(iconSimple);
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g); //To change body of generated methods, choose Tools | Templates.
+        if(isSelected()){
+            g.setColor(new Color(110, 213, 255));
+            g.fillRect(0, getHeight() - 3, getWidth(), getHeight());
+        }
+    }
+    
     
 }
